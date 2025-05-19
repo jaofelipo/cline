22bb6d3a5f76f9d96dd99c41a0e9732fcfde34e8
Jão Felipo <jaofelipo254@gmail.com>
2025-05-19 16:45:17 -0300

    Add ToolUseUtils to convert ToolUse to Webview message format

---
+++ src/core/task/ToolUseUtils.ts
@@ -0,0 +1,91 @@
+import { cwd } from "@/core/task";
+import { ToolUse } from "../assistant-message";
+import { BrowserAction, ClineAskQuestion, ClineAskUseMcpServer, ClineSayTool } from "../../shared/ExtensionMessage";
+import { getReadablePath, isLocatedInWorkspace } from "../../utils/path";
+import { parsePartialArrayString } from "@/shared/array";
+
+export function toJSON({name, params, partial}:ToolUse, content?:string, overrideTool?:any):string
+{
+	switch(name)
+	{
+		case 'browser_action':
+			if (params.action === 'launch')
+				return StringUtils.removeTag('url', params.url, partial)
+			else
+				return JSON.stringify({
+					action: params.action as BrowserAction,
+					coordinate: StringUtils.removeTag( "coordinate", params.coordinate, partial),
+					text: StringUtils.removeTag( "text", params.text, partial)})
+		case 'condense':
+			return StringUtils.removeTag("context", params.context, partial)
+		case 'plan_mode_respond':
+			return JSON.stringify({
+				response: StringUtils.removeTag("response", params.response, partial),
+				options: parsePartialArrayString(StringUtils.removeTag("options", params.options, partial)),
+				selected: content})
+		case 'new_task':
+			return StringUtils.removeTag("context", params.context, partial)
+		case "search_files":
+			return JSON.stringify({
+				tool: "searchFiles",
+				path: getReadablePath(cwd || '', StringUtils.removeTag("path", params.path, partial)),
+				regex: StringUtils.removeTag("regex", params.regex, partial),
+				filePattern: StringUtils.removeTag("file_pattern", params.file_pattern, partial),
+				content: content ?? '',
+				operationIsLocatedInWorkspace: isLocatedInWorkspace(params.path) })
+		case "use_mcp_tool":
+			return JSON.stringify({
+				type: "use_mcp_tool",
+				serverName: (content === undefined) ? StringUtils.removeTag("server_name", params.server_name) : params.server_name ?? "",
+				toolName: (content === undefined) ? StringUtils.removeTag("tool_name", params.tool_name) : params.tool_name,
+				arguments: (content === undefined) ? StringUtils.removeTag("arguments", params.arguments) : params.arguments})
+		case "list_code_definition_names"://list_code_definition_names
+			return toolToJSON('listCodeDefinitionNames', content ?? '', cwd, params.path, partial)
+		case 'list_files':
+			return toolToJSON((params.recursive?.toLowerCase() === "true") ? "listFilesRecursive" : "listFilesTopLevel", content ?? '', cwd, params.path, partial)
+		case 'read_file':
+			return toolToJSON('readFile', content ?? '', cwd, params.path, partial)
+		case 'write_to_file':
+		case 'replace_in_file':
+			return toolToJSON(overrideTool, content, cwd, params.path, partial)
+		case 'access_mcp_resource':
+			return JSON.stringify({
+				type:"access_mcp_resource",
+				serverName: StringUtils.removeTag("server_name", params.server_name, partial),
+				uri: StringUtils.removeTag("uri", params.uri, partial)
+			} satisfies ClineAskUseMcpServer)
+		case 'ask_followup_question':
+			return JSON.stringify({
+				question: StringUtils.removeTag("question", params.question, partial),
+				options: parsePartialArrayString(StringUtils.removeTag("options", params.options, partial)),
+				selected: content,
+			} satisfies ClineAskQuestion);
+		case 'execute_command':
+			return StringUtils.removeTag("command", params.command)
+	
+	}
+	return ''
+}
+
+
+function toolToJSON(tool: "editedExistingFile" | "newFileCreated" | "readFile"  | "listFilesRecursive" | "listFilesTopLevel" | 'listCodeDefinitionNames', 
+		content?: string, cwd?: string, path?: string, partial: boolean = false): string {
+	return JSON.stringify({
+		tool,
+		path: getReadablePath(cwd || '', StringUtils.removeTag("path", path, partial)),
+		content,
+		operationIsLocatedInWorkspace: isLocatedInWorkspace(path)
+	} satisfies ClineSayTool)
+}
+
+
+export function parseJSON(text?:string)
+{
+	if (!text)
+		return {}
+	try { 
+		return JSON.parse(text)
+	} catch { 
+		return undefined
+	}
+}
---
+++ src/utils/StringUtils.ts
@@ -0,0 +1,79 @@
+class StringUtils 
+{
+    static removeStart(newContent: string, delimiter:string = "```"): string 
+    {
+      return (newContent.startsWith(delimiter)) ? newContent.split("\n").slice(1).join("\n").trim() : newContent
+    }
+  
+    static removeEnd(newContent: string, delimiter:string = "```"): string 
+    {
+      return newContent.endsWith(delimiter) ? newContent.split("\n").slice(0, -1).join("\n").trim() : newContent;
+    }
+
+    static removeStartAndEnd(newContent: string, start:string = "```", end:string = "```"): string 
+    {
+        newContent = (newContent.startsWith(start)) ? newContent.split("\n").slice(1).join("\n").trim() : newContent
+        return newContent.endsWith(end) ? newContent.split("\n").slice(0, -1).join("\n").trim() : newContent;
+    }
+
+    /**
+     * Cleans text by converting HTML entities to their corresponding characters
+     * removes Unicode replacement characters (�) and ``` ´´´ at begin and end
+     * 
+     * @param text - Text to be sanitized
+     * @returns Cleaned text without HTML entities and replacement characters
+     */
+    static fixModelContent(text: string): string 
+    {
+        text = StringUtils.removeStartAndEnd(text)        
+        return StringUtils.convertHtmlEntitiesToText(text).replace(/\uFFFD/g, "")
+    }
+    
+    static htmlEntitiesMap = {
+        '&gt;': '>',
+        '&lt;': '<',
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'"
+    }
+    
+    /** Convert &gt &lt &quot &amp &apos em texto equivalente */ 
+    static convertHtmlEntitiesToText(newContent: string): string 
+    {
+        return newContent.replace(/&gt;|&lt;|&quot;|&amp;|&apos;/g, (match) => 
+            StringUtils.htmlEntitiesMap[match as keyof typeof StringUtils.htmlEntitiesMap] || match);
+    }
+
+    /**
+     * Cleans text by converting HTML entities to their corresponding characters
+     * and removes Unicode replacement characters (�)
+     * 
+     * @param text - Text to be sanitized
+     * @returns Cleaned text without HTML entities and replacement characters
+     */
+    static fixModelDiffContent(text: string): string 
+    {
+        return StringUtils.convertHtmlEntitiesToText(text).replace(/\uFFFD/g, "")
+    }
+
+    static regexTagCache = new Map<string, RegExp>();
+    
+    /**
+     * If block is partial, remove partial closing tag so its not presented to user
+     */
+    static removeTag(tag: string, text?: string, partial: boolean = true) 
+    {
+        if (partial && text) // Dynamically constructs a regex to match closing tags, including optional whitespace, '<' or '</', and tag name.
+        {
+            let regex = StringUtils.regexTagCache.get(tag);
+            if (!regex) 
+            {
+                regex = new RegExp(`\\s?<\/?${tag.split("").map((char) => `(?:${char})?`).join("")}$`, "g");
+                StringUtils.regexTagCache.set(tag, regex);
+            }    
+            return text.replace(regex, "")
+        }
+        return text || ""
+    }
+    
+}
