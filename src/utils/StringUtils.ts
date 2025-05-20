454f55e8e9cd9a3303599dcd2ecbcb4fbef1914a
Jão Felipo <jaofelipo254@gmail.com>
2025-05-20 01:44:30 -0300

    feat: Add string comparison utility function to the `StringUtils` class

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -75,5 +75,22 @@ class StringUtils
         }
         return text || ""
     }
+
+    static compareStrings(str1: string, str2: string): string 
+    {
+        const maxLength = Math.max(str1.length, str2.length)
+        let resultado = ''
+        for (let i = 0; i < maxLength; i++) 
+        {
+            const char1 = str1[i] || ''
+            const char2 = str2[i] || '' 
+    
+            if (char1 !== char2) 
+                resultado += `[${char1 || ' '}:${char2 || ' '}]`
+            else 
+                resultado += char1
+        }
+        return resultado
+    }
     
 }
